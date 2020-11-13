0f6235204e77d5bd391844a216bf8fa5099e8aab
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-12 22:01:45 -0500

    create reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,24 +1,20 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :update, :destroy]
 
-  # GET /reviews
+  # GET products/:product_id/reviews
   def index
-    @reviews = Review.all
+    @reviews = Review.where([product_id: params[:product_id]])
 
     render json: @reviews
-  end
-
-  # GET /reviews/1
-  def show
-    render json: @review
   end
 
   # POST /reviews
   def create
     @review = Review.new(review_params)
+    @review.product_id = params[:product_id]
 
     if @review.save
-      render json: @review, status: :created, location: @review
+      render json: @review, status: :created
     else
       render json: @review.errors, status: :unprocessable_entity
     end
