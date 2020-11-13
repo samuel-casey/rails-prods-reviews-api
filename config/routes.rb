bbe3a3eb88469931c5fb3d75cfbce16fe544affb
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-12 21:32:50 -0500

    routes trimmed with only and except

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :products do
-    resources :reviews
+  resources :products, only: [:index, :show] do
+    resources :reviews, except: [:show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
