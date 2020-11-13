c16a0fa8a0cdaa23cdab7ec9ac8196ece7b3cb01
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-12 22:06:45 -0500

    added update, destroy for reviews'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :products, only: [:index, :show] do
-    resources :reviews, except: [:show]
+    resources :reviews, except: [:show, :update, :destroy]
   end
+
+  resources :reviews, only: [:update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
